d488ca584bc5b04c94021d7bc74165adf46d71e7
MilioneriNomer1 <eva.tihova2005@gmail.com>
2023-08-07 17:06:39 +0300

    Working Dialogs

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,39 +1,75 @@
- import { Component, OnInit } from '@angular/core';
- import {County} from "../../common/County";
- import {Product} from "../../common/Product";
- import {CountyService} from "../../services/county.service";
- import {ProductService} from "../../services/product.service";
- import {LazyLoadEvent} from "primeng/api";
- @Component({
-   selector: 'app-product',
-   templateUrl: './product.component.html',
-   styleUrls: ['./product.component.scss']
- })
- export class ProductComponent implements OnInit {
+import { Component, OnInit } from '@angular/core';
+import {Product} from "../../common/Product";
+import {ProductService} from "../../services/product.service";
+import {ConfirmationService, LazyLoadEvent, MessageService} from "primeng/api";
+import {FieldInfo} from "../../FieldInfo";
 
-   products: Product[] = [];
-   totalRecords: number = 0;
-   loading: boolean = false;
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
 
-   constructor(private productService: ProductService) { }
+  fields: FieldInfo[] = [];
 
-   ngOnInit(): void {
-     this.loading = true;
-   }
+  totalRecords: number = 0;
+  loading: boolean = false;
+  productDialog: any;
+  product: Product = new Product();
+  submitted: boolean = false;
 
-   loadProducts(event: LazyLoadEvent) {
-     this.loading = true;
-     debugger
-     setTimeout(() => {
-       // @ts-ignore
-       this.productService.getProduct({lazyEvent: JSON.stringify(event)}).then(res => {
-         // @ts-ignore
-         this.products = res.slice(event.first,event.rows + event.first);
-         this.totalRecords = res.length;
-         this.loading = false;
-       })
-     }, 1000);
-   }
+  constructor(public productService: ProductService, private messageService: MessageService, private confirmationService: ConfirmationService) { }
 
- }
+  ngOnInit(): void {
+    this.loading = true;
+    this.fields = [
+      {
+        name: 'name',
+        title: 'Name'
+      },
+      {
+        name: 'itemId',
+        title: 'Item Id'
+      },
+    ];
+  }
+
+  openNew() {
+    this.product = new Product();
+    this.submitted = false;
+    this.productDialog = true;
+  }
+
+  edit(product: any) {
+    this.product = {...product};
+    this.productDialog = true;
+    this.submitted = false;
+
+  }
+
+  delete(product: any) {
+    this.confirmationService.confirm({
+      message: 'Are you sure you want to delete ' + product.name + '?',
+      header: 'Confirm',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.messageService.add({severity:'warn', summary: 'Successful', detail: 'Product Deleted', life: 3000})
+      }
+    });
+
+  }
+
+  hideDialog() {
+    this.productDialog = false;
+    this.submitted = false;
+
+  }
+
+  saveProduct() {
+    this.messageService.add({severity:'success', summary: 'Successful', detail: 'Product Updated', life: 3000});
+    this.productDialog = false;
+  }
+}
+
 
